476682871f6ea5dcd42b44848128b6dc8760d1ef
Radi A <radi.a@live.com.au>
2017-01-19 13:17:22 +0200

    Added typings

---
+++ typings/browser.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="browser/ambient/angular-mocks/index.d.ts" />
+/// <reference path="browser/ambient/angular-route/index.d.ts" />
+/// <reference path="browser/ambient/angular/index.d.ts" />
+/// <reference path="browser/ambient/jquery/index.d.ts" />
+/// <reference path="browser/ambient/mocha/index.d.ts" />
+/// <reference path="browser/ambient/require/index.d.ts" />
+/// <reference path="browser/ambient/ui-grid/index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="main/ambient/angular-mocks/index.d.ts" />
+/// <reference path="main/ambient/angular-route/index.d.ts" />
+/// <reference path="main/ambient/angular/index.d.ts" />
+/// <reference path="main/ambient/jquery/index.d.ts" />
+/// <reference path="main/ambient/mocha/index.d.ts" />
+/// <reference path="main/ambient/require/index.d.ts" />
+/// <reference path="main/ambient/ui-grid/index.d.ts" />
